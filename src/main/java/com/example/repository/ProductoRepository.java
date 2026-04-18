2da54c159123fd393a13394d34c6d25856a7e1cb
Andres Vega <andresvegamunoz2206@gmail.com>
2026-04-17 19:28:28 -0600

    Add ProductoRepository interface

---
+++ src/main/java/com/example/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
