47686adc1498dc3a983e50a0626ce5ff59e0127f
Andres Vega <andresvegamunoz2206@gmail.com>
2026-04-17 19:28:11 -0600

    Create Producto.java model class

---
+++ src/main/java/com/example/model/Producto.java
@@ -0,0 +1,26 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String descripcion;
+
+    private Double precio;
+
+    private Integer cantidad;
+}
